912b248514a067779527a42566a94965ca1fe307
puerarongo <akhardin96@gmail.com>
2022-02-26 22:40:37 +0300

    add fetchCountries function

---
+++ src/function__js/fetchCountries.js
@@ -0,0 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const listEl = document.querySelector(".country-list");
+const infoEl = document.querySelector(".country-info");
+
+
+// todo FUNCTION
+export const fetchCountries = (name) => {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(responce => { return responce.json() })
+        .then(country => {
+        if (country.length > 10) {
+            Notify.info("Too many matches found. Please enter a more specific name!");
+        }
+        else if (country.length > 2 && country.length < 10) {
+            countryList(country);
+        }
+        else if (country.length === 1) {
+            countryInfo(country);
+        }
+        console.log(country[0].languages)
+        }) // country[0] - доступ к объекту
+        .catch(error => Notify.failure("Oops, there is no country with that name!"));
+};
+
+const countryList = (countryName) => {
+    let listCountryName = countryName.map((elem) => `<li><img src="${elem.flags.svg}" 
+    alt="country flag">${elem.name.common}</li>`).join("");
+
+    listEl.insertAdjacentHTML("beforeend", listCountryName);
+};
+
+const countryInfo = (countryName) => { 
+    let infoCountryName = `<div><img src="${countryName[0].flags.svg}" 
+    alt="country flag"><h1>${countryName[0].name.official}</h1></div>
+    <ul><li><p>Capital: ${countryName[0].capital}</p></li>
+    <li><p>Population: ${countryName[0].population}</p></li>
+    <li><p>Languages: ${Object.values(countryName[0].languages).join(", ")}</p></li></ul>`
+
+    infoEl.insertAdjacentHTML("beforeend", infoCountryName);
+};
+// todo
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './function__js/fetchCountries';
+//import trim from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
+let inputValue = null;
+
+// **************************************************************
+
+const inputEl = document.querySelector("#search-box");
+
+
+const changeInput = (event) => {
+    inputValue = event.target.value;
+
+    fetchCountries(inputValue);
+};
+
+
+
+
+
+inputEl.addEventListener("input", debounce(changeInput, DEBOUNCE_DELAY));
